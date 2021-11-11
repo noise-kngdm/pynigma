7dc5fc9a95874be2dea9bfdcfb1889b2d2a93c6e
Luis Javier Soriano Suarez <e.luissoriano@go.ugr.es>
2021-11-11 14:04:30 +0100

    Put common methods in base enigma class (#2)

--- pynigma/rotor.py
+++ pynigma/rotor.py
@@ -1,39 +1,51 @@
+import sys
 import constants
+from typing import List
+from base_enigma import Base
 
-class Rotor():
+
+class Rotor(Base):
     """
     Module that represent a Rotor or Walzenlage
     """
-    def _check_valid_number(self, num: int):
+    def _check_keys_and_values(self, keys, values):
         """
-        Checks that the number passed as a parameter is a valid one.
+        Check that a key's value is not equal to the key.
 
         Parameters
         ----------
-        num : int
-            Number that will be checked.
+        keys : list of int
+            A list with all the keys used.
+        values : list of int
+            A list with all the values used.
 
         Raises
         ------
         ValueError
+            If a key's value is equal to the key.
+        ValueError
             If the number is not in the expected range.
         """
-        if num < constants.MIN_NUM or num > constants.MAX_NUM:
-            raise ValueError(f'The value of {num} must be between '
-                             f'{constants.MIN_NUM} and {constants.MAX_NUM}')
+        super()._check_keys_and_values(keys, values)
 
-    def __init__(self, ringstellung, grundstellung, permutations: list[list[int, int]]):
+        for i in range(len(keys)):
+            if keys[i] == values[i]:
+                raise ValueError('Key and value cannot be equal')
+
+    def __init__(self, ringstellung, grundstellung, notch, permutations):
         """
             Constructor of the Rotor class.
 
             Parameters
             ----------
-            ringstellung : str
-                Letter that configure the rotor's ring
-            grundstellung : str
-                Letter that is configure to be the initial letter of the rotor
-            permutations : list[tuple[int]]
-                A nested list of numbers between in the [1-26] range, simulating a dictionary.
+            ringstellung : int
+                Number that equals to a letter that configure the rotor's ring
+            grundstellung : int
+                Number that equals to a letter that is configure to be the initial letter of the rotor
+            notch : int
+                Number which his function is to indicate to his left rotor to do a rotation
+            permutations : list[tuple[int, int]]
+                A list of tuples with numbers between in the [1-26] range.
 
             Raises
             ------
@@ -44,7 +56,45 @@ class Rotor():
             ValueError
                 If permutation has more than 26 items
         """
-        if len(permutations) > constants.MAX_NUM:
-            raise TypeError('The max number of pairs that can be passed to'
-                            f'{sys._getframe(1).f_code.co_name} is '
-                            f'{constants.MAX_NUM_PAIRS_PLUGBOARD}')
+        super().__init__(permutations)
+
+        if len(permutations) != constants.MAX_NUM:
+            raise TypeError('The number of elements that must to be passed to'
+                            f'{sys._getframe(4).f_code.co_name} is '
+                            f'{constants.MAX_NUM}')
+
+        self._check_valid_number(ringstellung)
+        self._ringstellung = ringstellung
+    
+        self._check_valid_number(notch)
+        self._notch = notch
+
+        self._check_valid_number(grundstellung)
+        self._grundstellung = grundstellung
+
+    def _set_key(self, x: int, y: int):
+        """
+        Set the values on _permutations.
+
+        Parameters
+        ----------
+        x : int
+            A number.
+        y : int
+            A number.
+        """
+        self._permutations[x] = y
+
+def num_of(cad):
+        return ord(cad)-ord('A') + 1
+
+if __name__ == '__main__':
+
+    k = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    l = "ZYXWVUTSRQPONMLKJIHGFEDCBA"
+
+    lista = [(num_of(k[i]),num_of(l[i])) for i in range(constants.MAX_NUM)]
+            
+    print(lista)
+
+    rotor_1 = Rotor(10,10,10,lista)
